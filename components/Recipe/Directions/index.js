eb38ede7df4ddb4c5a35562dcf0118391fe9b2e5
Christina <christikaes@gmail.com>
2016-02-28 14:42:35 -0500

    ingredients and directions are savable

--- components/Recipe/Directions/index.js
+++ components/Recipe/Directions/index.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { Component, PropTypes, setState } from 'react'
+import {bindActionCreators} from 'redux'
+import { connect } from 'react-redux'
+import { update } from '../../../actions'
+import ContentEditable from 'react-contenteditable'
+import styles from './styles.css'
 
 const RecipeDirections = React.createClass({
-  render: function() {
-    return (
-    	<div>
-      		Directions
-    	</div>
-    )
-  }
-})
+  updateDirections: function(evt){
+    const {update} = this.props;
+    update("directions", evt.target.value);
+  },
 
-export default RecipeDirections;
+  render: function(){
+    const {recipe} = this.props;
+    return(
+      <div className={styles.container}>
+        <ContentEditable  className={styles.directions} html={recipe.directions} disabled={false} onChange={this.updateDirections} />
+      </div>) 
+  }
+});
+
+RecipeDirections.propTypes = {
+  update: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    recipe: state.recipe.present
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ update }, dispatch);
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(RecipeDirections)
--- components/Recipe/Ingredients/index.js
+++ components/Recipe/Ingredients/index.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { Component, PropTypes, setState } from 'react'
+import {bindActionCreators} from 'redux'
+import { connect } from 'react-redux'
+import { update } from '../../../actions'
+import ContentEditable from 'react-contenteditable'
+import styles from './styles.css'
 
 const RecipeIngredients = React.createClass({
-  render: function() {
-    return (
-    	<div>
-      		Ingredients
-      	</div>
-    )
-  }
-})
+  updateIngredients: function(evt){
+    const {update} = this.props;
+    update("ingredients", evt.target.value);
+  },
 
-export default RecipeIngredients;
+  render: function(){
+    const {recipe} = this.props;
+    return(
+      <div className={styles.container}>
+        <ContentEditable  className={styles.ingredients} html={recipe.ingredients} disabled={false} onChange={this.updateIngredients} />
+      </div>) 
+  }
+});
+
+RecipeIngredients.propTypes = {
+  update: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    recipe: state.recipe.present
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ update }, dispatch);
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(RecipeIngredients)
