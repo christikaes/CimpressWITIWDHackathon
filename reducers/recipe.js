08992011ad691701ff51261d27806b476e89db2c
Christina <christikaes@gmail.com>
2016-02-28 19:36:58 -0500

    changed link to ec2

--- reducers/recipe.js
+++ reducers/recipe.js
@@ -13,8 +13,8 @@ const save = function(state) {
 	
 	// TODO: Is there a way to make this a relative path?
 
-	client.post("http://localhost:3000/recipe-form", args, function (data, response) {
-	// client.post("http://ec2-52-26-116-243.us-west-2.compute.amazonaws.com:3000/recipe-form", args, function (data, response) {
+	// client.post("http://localhost:3000/recipe-form", args, function (data, response) {
+	client.post("http://ec2-52-26-116-243.us-west-2.compute.amazonaws.com:3000/recipe-form", args, function (data, response) {
 		// parsed response body as js object 
 		console.log(data);
 		// raw response 
@@ -28,7 +28,8 @@ const save = function(state) {
 	}
 
 	// direct way 
-	client.get("http://localhost:3000/recipe-saved", args, function (data, response) {
+	client.get("http://ec2-52-26-116-243.us-west-2.compute.amazonaws.com:3000/recipe-saved", args, function (data, response) {
+	// client.get("http://localhost:3000/recipe-saved", args, function (data, response) {
 		// parsed response body as js object 
 		console.log(data);
 		// raw response 
