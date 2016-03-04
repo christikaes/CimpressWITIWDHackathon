bba34e43ed11a42a5fee48cda2c6ab65b486584e
snarayan <snarayan@vistaprint.com>
2016-03-04 18:41:07 -0500

    Fixed image paths on home page

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {Grid, Row, Col, Button} from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
 
+var img = new Image();
+
 const Home = React.createClass({
   render: function() {
     return (
 	<Grid>
 	    <Row>
 	    	<Col xs={4}>
-	    		<img id="logo" class="two-columns" src="/img/CWIT_Logo-Cimpress.png" alt="WIT_logo" />
+	    		<img id="logo" class="two-columns" src={require("../../node_modules/image/CWIT_Logo-Cimpress.png")} alt="WIT_logo" />
 	    	</Col>
 	    	<Col xs={8}>
 	    		<h1>Women In Technology Celebrates International Women's Day!</h1>
@@ -18,8 +20,7 @@ const Home = React.createClass({
 	      <Col xs={12}>
 	      	  <h2>International Women's Day is on <span>March 8th!</span></h2>		  
 			  <p>To celebrate, we will be creating a global cookbook consisting of recipes submitted by WIT members from Cimpress offices all over the world.
-			  Everyone who submits a Recipe or a Tech Page will get a copy of the cookbook!
-			  Anyone who submits a Tech Page will be entered in a raffle.
+			  Everyone who submits a Recipe or a Tech Page will get a copy of the cookbook and will also be entered into a raffle.
 			  </p>
 	      </Col>
 	    </Row>
@@ -32,7 +33,7 @@ const Home = React.createClass({
 	    </Row>
 	    <Row>
 	      <Col xs={12} md={6}>
-	          <img src="/img/recipe.png" alt="Generic placeholder image" width="140" height="140" />
+	          <img src={require("../../node_modules/image/recipe.png")} alt="Recipe image" width="140" height="140" />
 	          <h2>Recipe</h2>
 	          <p>Submit your favorite recipe, a unique cultural specialty, or something for fun! </p>
 	          <LinkContainer to="/recipe">
@@ -40,7 +41,7 @@ const Home = React.createClass({
 	          </LinkContainer>
 	      </Col>
 	      <Col xs={12} md={6}>
-	          <img src="/img/puzzle.png" alt="Generic placeholder image" width="140" height="140" />
+	          <img src={require("../../node_modules/image/puzzle.png")} alt="Puzzle image" width="140" height="140" />
 	          <h2 class="feature-heading">Tech Page</h2>
 	          <p class="lead">Submit an interesting article on an innovative technology or a fun yet challenging puzzle!</p>
 	          <LinkContainer to="/tech">
