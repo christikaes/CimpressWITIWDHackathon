a4467864abb500602abbf230174db98e6d53e1e5
PikoLina <annwong03@gmail.com>
2016-03-06 22:58:01 -0500

    cleaned up the homepage with content and updated image paths to fix them.

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -13,7 +13,7 @@ const Home = React.createClass({
 	<Grid>
 	    <Row>
 	    	<Col xs={1}>
-	    		<img id="logo" src={require("../../node_modules/image/CWIT_Logo-Cimpress.png")} alt="WIT_logo" width="70" height="100" />
+	    		<img id="logo" src={require("../../img/CWIT_Logo-Cimpress.png")} alt="WIT_logo" width="70" height="100" />
 	    	</Col>
 	    	<Col xs={11}>
 	    		<h1>Women In Technology Celebrates International Women's Day!</h1>
@@ -22,8 +22,16 @@ const Home = React.createClass({
 	    <Row>
 	      <Col xs={12}>
 	      	  <h2>International Women's Day is on <span>March 8th!</span></h2>		  
-			  <p>To celebrate, we will be creating a global cookbook consisting of recipes submitted by WIT members from Cimpress offices all over the world.
-			  Everyone who submits a Recipe or a Tech Page will get a copy of the cookbook and will also be entered into a raffle.
+			  <p>
+			  To celebrate in all of our international offices, WIT will be building a global cookbook comprising of recipes from around the world. 
+			  To accomplish this, we need submissions by WIT members and their supporters from all of our offices. And to make it more fun, we are also requesting submissions for a TechPage.
+			  </p>
+			  <p>
+			  With a submission, you will receive an entry to one of our raffles as well as a copy of the finalized cookbook! We will hold a raffle for Recipe submissions and another for Tech Page submissions.
+			  Multiple submissions are allowed and you will receive an additional raffle entry for the category that you had submitted to. The winners of each raffle will win a trip to the upcoming Grace Hopper Conference in Houston Texas!* 
+			  </p>
+			  <p>
+			  <b> Note: Please keep submissions in English. If you need help in translating your recipe or tech page prior to submission, contact the <a href="mailto:WITExternalConnectionsCommitee@cimpress.com?Subject=WIT%20IWD%20Question" target="_top">WITExternalConnectionsCommitee</a> . </b>
 			  </p>
 	      </Col>
 	    </Row>
@@ -36,8 +44,9 @@ const Home = React.createClass({
 	      </Col>
 	    </Row>
 	    <Row>
+		<br/>
 	      <Col xs={12} md={6}>
-	          <img src={require("../../node_modules/image/recipe.png")} alt="Recipe image" width="140" height="140" />
+	          <img src={require("../../img/recipe.png")} alt="Recipe image" width="140" height="140" />
 	          <h2>Recipe</h2>
 	          <p>Submit your favorite recipe, a unique cultural specialty, or something for fun! </p>
 	          <LinkContainer to="/recipe">
@@ -45,7 +54,7 @@ const Home = React.createClass({
 	          </LinkContainer>
 	      </Col>
 	      <Col xs={12} md={6}>
-	          <img src={require("../../node_modules/image/puzzle.png")} alt="Puzzle image" width="140" height="140" />
+	          <img src={require("../../img/puzzle.png")} alt="Puzzle image" width="140" height="140" />
 	          <h2>Tech Page</h2>
 	          <p>Submit an interesting article on an innovative technology or a fun yet challenging puzzle!</p>
 	          <LinkContainer to="/tech">
@@ -53,6 +62,15 @@ const Home = React.createClass({
 	          </LinkContainer>
 	      </Col>
 	    </Row>
+		<Row>
+			<Col>
+			<br/> <hr/>
+			<p>
+			*To learn more about the Grace Hopper Conference, visit the <a href="http://ghc.anitaborg.org/" target="_top">main site</a>. The trip to the Grace Hopper Conference will include admission to 3 days of the conference, hotel, and flight. <br/>
+			For more details email the <a href="mailto:WITExternalConnectionsCommitee@cimpress.com?Subject=WIT%20IWD%20Question" target="_top">WITExternalConnectionsCommitee</a>.
+			</p>
+			</Col>
+		</Row>
 	</Grid>);
   }
 })
