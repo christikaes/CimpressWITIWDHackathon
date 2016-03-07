72cce74f99af5ab965f87b54bff0c8612d11ff22
Christina <christikaes@gmail.com>
2016-03-07 01:45:44 -0500

    port change test

--- containers/App/index.js
+++ containers/App/index.js
@@ -9,6 +9,7 @@ const App = React.createClass({
     return (
       <div>
         <a href="http://FreeHTMLtoPDF.com/?convert">Download as PDF</a>
+        <a href="http://api.html2pdfrocket.com/pdf?value=http://ec2-52-26-116-243.us-west-2.compute.amazonaws.com:3000&apikey=ef82f568-7282-4d99-adf0-7f2b936f20ae">Download PDF</a>
         <div>
           {this.props.children}
         </div>
